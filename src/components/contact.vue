6b5bcb35f89968da00ab30b9fbea72cb97114070
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-15 18:24:21 +0900

    お問い合わせページの編集

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Contact</h1>
+  <div>
+    <div class="container mt-5 px-5">
+      <h1>Contact</h1>
+      <div>
+        <p class="mt-4">お問い合わせは以下へお願いいたします。</p>
+        <div class="mt-5">
+          <p>メールアドレス</p>
+          <p><span style="font-weight: bold;">t.yoshino0331@gmail.com</span></p>
+        </div>
+        <p class="my-5">or</p>
+        <div>
+          <p>Twitterのダイレクトメッセージ</p>
+          <a href="https://twitter.com/yoshino_t0331" class="btn" style="background: #1da1f2; color: white; font-weight: bold;">Twitter</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
