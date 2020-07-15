15bcd25e0eee7eed8c3aeda724d507524f664af7
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-15 12:54:24 +0900

    使用言語ページの大枠を記述

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -1,3 +1,59 @@
 <template>
-    <h1>Skills</h1>
+  <div>
+    <div class="container mt-5 px-5">
+      <h1>Skills</h1>
+      <div class="row mt-3">
+        <!-- HTML/CSS -->
+        <!-- スペース -->
+        <div class="col-lg-1 float-left p-0 my-4 sp-block"></div>
+        <!-- HTML -->
+        <div class="card col-lg-5 float-left p-0 my-4">
+          <div style="height:48px; background-color:#e44b23;"></div>
+          <div class="card-body">
+            <h4>HTML</h4>
+            <p>	Framework/Library: Haml</p>
+            <p class="card-text text-left">
+
+            </p>
+          </div>
+        </div>
+        <!-- CSS -->
+        <div class="card col-lg-5 float-left p-0 my-4">
+          <div style="height:48px; background-color:#563d7c;"></div>
+          <div class="card-body">
+            <h4>CSS</h4>
+            <p>Framework/Library: SCSS,Bootstrap</p>
+            <p class="card-text text-left">ああああ</p>
+          </div>
+        </div>
+        <!-- スペース -->
+        <div class="col-lg-1 float-left p-0 my-4 sp-block"></div>
+        <!-- ここまでで1列 -->
+        <!-- Ruby/JavaScript -->
+        <!-- スペース -->
+        <div class="col-lg-1 float-left p-0 my-4 sp-block"></div>
+        <!-- Ruby -->
+        <div class="card col-lg-5 float-left p-0 my-4">
+          <div style="height:48px; background-color:#701516;"></div>
+          <div class="card-body">
+            <h4>Ruby</h4>
+            <p>Framework/Library: Ruby on Rails</p>
+            <p class="card-text text-left">ああああ</p>
+          </div>
+        </div>
+        <!-- JavaScript -->
+        <div class="card col-lg-5 float-left p-0 my-4">
+          <div style="height:48px; background-color:#f1e05a;"></div>
+          <div class="card-body">
+            <h4>JavaScript</h4>
+            <p>Framework/Library: jQuery,Vue.js</p>
+            <p class="card-text text-left">ああああ</p>
+          </div>
+        </div>
+        <!-- スペース -->
+        <div class="col-lg-1 float-left p-0 my-4 sp-block"></div>
+        <!-- ここまでで1列 -->
+      </div>
+    </div>
+  </div>
 </template>
