3a82dafa4eff3e40a0809fa5c59a83c7064b839f
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-17 18:04:44 +0900

    現時点でのポートフォリオを記述

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="portfolio">
     <div class="container mt-5 px-5">
-      <h1 class="mb-5">My Portfolio</h1>
+      <h1 class="mb-3">My Portfolio</h1>
+      <div class="mb-5">
+        <a href="#motivate-yourself" class="btn btn-outline-dark mx-1">MotivateYourself</a>
+        <a href="#portfolio-site" class="btn btn-outline-dark mx-1">ProfileSite</a>
+        <a href="#secretary-bot" class="btn btn-outline-dark mx-1">SecretaryBot</a>
+      </div>
       <!-- ここから1ブロック -->
       <div class="mt-3 mx-5 Box">
-        <h2 class="py-1 portfolio__name">Motivate Yourself</h2>
+        <h2 class="py-1 portfolio__name"><a id="motivate-yourself">Motivate Yourself</a></h2>
         <div class="py-3 px-5 border-black">
           <h3>主な使用技術</h3>
           <h4>Ruby on Rails,JavaScript,AWS,MySQL</h4>
@@ -21,13 +26,43 @@
       </div>
       <!-- ここまで1ブロック -->
       <!-- ここから1ブロック -->
-      <div class="mt-3 mx-5  Box">
+      <div class="mt-3 mx-5 Box">
+        <h2 class="py-1 portfolio__name"><a id="portfolio-site">My Profile Site</a></h2>
+        <div class="py-3  px-5 border-black">
+          <h3>主な使用技術</h3>
+          <h4>Vue.js,Bootstrap,FireBase</h4>
+          <h3 class="pt-3">概要</h3>
+          <h5 class="portfolio__content pb-3">
+            現在ご覧いただいているサイトです。SPAを作りたいと考えて手始めとして実装いたしました。
+            </h5>
+          <a href="https://github.com/tomoyayoshino/My-Vue-ProfileSite" class="btn px-5 ml-4" style="background: #24292e; color: white; font-weight: bold;">GitHub</a>
+          <a href="https://www.google.com/?hl=ja" class="btn btn-info ml-3" style="color: white; font-weight: bold;">アクセスはこちらから</a>
+          <h3 class="pt-4">デモムービー</h3>
+        </div>
+      </div>
+      <!-- ここまで1ブロック -->
+      <!-- ここから1ブロック -->
+      <div class="mt-3 mx-5 Box" id="secretary-bot">
+        <h2 class="py-1 portfolio__name">Secretary Bot</h2>
+        <div class="py-3  px-5 border-black">
+          <h3>主な使用技術</h3>
+          <h4>LINE Messaging API,Google Apps Script</h4>
+          <h3 class="pt-3">概要</h3>
+          <h5 class="portfolio__content pb-3">
+            Googleカレンダーへの予定の追加・参照と、決まった時間に今日・明日の予定を通知するbotです。簡単な挨拶への返答機能もあります。
+            </h5>
+          <h3 class="pt-4">デモムービー</h3>
+        </div>
+      </div>
+      <!-- ここまで1ブロック -->
+      <!-- ここから1ブロック -->
+      <div class="mt-3 mx-5 Box">
         <h2 class="py-1 portfolio__name">My Profile Site</h2>
         <div class="py-3  px-5 border-black">
           <h3>主な使用技術</h3>
           <h4>Vue.js,Bootstrap,FireBase</h4>
           <h3 class="pt-3">概要</h3>
-          <h4 class="portfolio__conten pb-3">このサイトです</h4>
+          <h5 class="portfolio__conten pb-3">このサイトです</h5>
           <a href="https://github.com/tomoyayoshino/My-Vue-ProfileSite" class="btn px-5 ml-4" style="background: #24292e; color: white; font-weight: bold;">GitHub</a>
           <a href="https://www.google.com/?hl=ja" class="btn btn-info ml-3" style="color: white; font-weight: bold;">アクセスはこちらから</a>
           <h3 class="pt-4">デモムービー</h3>
@@ -45,6 +80,7 @@
     &__name{
       background: darkgray;
       color: white;
+      text-shadow: 0 0 6px rgba(0,0,0,0.8);
     }
     &__content{
       text-align-last: start;
@@ -54,3 +90,11 @@
     border-bottom: 1px solid gray;
   }
 </style>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+  }
+}
+</script>
